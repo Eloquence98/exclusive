94f2836e72ac54d1f5cf1693804115eb312bbcdd
Eloquence98 <ibneenayat1998@gmail.com>
2024-07-02 03:18:32 +0500

    feat: setup auth.js sign in with google

---
+++ app/_lib/auth.js
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    // if using credentials
+    // Credentials({
+    //   credentials: {
+    //     username: { label: "Username" },
+    //     password: { label: "Password", type: "password" },
+    //   },
+    //   async authorize({ request }) {
+    //     const response = await fetch(request);
+    //     if (!response.ok) return null;
+    //     return (await response.json()) ?? null;
+    //   },
+    // }),
+  ],
+};
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth(authConfig);
