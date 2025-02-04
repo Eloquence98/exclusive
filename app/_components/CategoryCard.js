74b64597b3f4a3e3476006e9bd71a9c47ac2b984
Eloquence98 <ibneenayat1998@gmail.com>
2025-02-05 03:43:08 +0500

    fix: add links on browse by category section

--- app/_components/CategoryCard.js
+++ app/_components/CategoryCard.js
@@ -1,12 +1,19 @@
+import Link from "next/link";
 import React from "react";
 import { cloneElement } from "react";
 
-function CategoryCard({ category: { name, icon } }) {
+function CategoryCard({ category: { name, pathname, query, icon } }) {
   return (
-    <div className="transition-default flex flex-col items-center justify-center rounded border border-border bg-white py-4 text-black hover:border-primary hover:bg-primary hover:text-white">
+    <Link
+      href={{
+        pathname,
+        query: { category: query },
+      }}
+      className="transition-default flex flex-col items-center justify-center rounded border border-border bg-white py-4 text-black hover:border-primary hover:bg-primary hover:text-white"
+    >
       {cloneElement(icon, { className: "mb-4 h-12 w-12" })}
       <p>{name}</p>
-    </div>
+    </Link>
   );
 }
 
