0cbc5c1344291ed2a919100a5c7081b1a4227df1
Eloquence98 <ibneenayat1998@gmail.com>
2025-02-10 18:08:30 +0500

    fix: resolve repetitive content on body

--- app/layout.js
+++ app/layout.js
@@ -30,13 +30,6 @@ export default function RootLayout({ children }) {
           </main>
           <Footer />
         </Providers>
-        <Providers>
-          <Header />
-          <main className="mb-24 min-h-[calc(100dvh-4.625rem)]">
-            {children}
-          </main>
-          <Footer />
-        </Providers>
       </body>
     </html>
   );
