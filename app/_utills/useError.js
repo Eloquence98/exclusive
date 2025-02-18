78f35d6ed0c1103b649a4d865f824345503ae1ce
Eloquence98 <ibneenayat1998@gmail.com>
2025-02-18 18:20:13 +0500

    lost in the woods

--- app/_utills/useError.js
+++
@@ -1,8 +0,0 @@
-import { useState, useRef } from "react";
-
-export function useError() {
-  const [error, setError] = useState("");
-  const ref = useRef(null);
-
-  return [error, setError, ref];
-}
