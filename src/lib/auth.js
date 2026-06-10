77daa18a71619112e80aaf7aa7c2c5f51e6851a5
Eloquence98 <ibneenayat1998@gmail.com>
2026-06-11 02:58:03 +0500

    fix auth.js imports

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,6 @@
-// import NextAuth from "next-auth";
-// import Google from "next-auth/providers/google";
-// import Credentials from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
 // const authConfig = {
 //   providers: [
